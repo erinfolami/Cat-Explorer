4e4ee936736d83b215bcb4d317f52f6e974c2ccf
erinfolami <emmanuelerinfolami2@gmail.com>
2022-08-12 10:26:06 +0100

    added System UI Controller dependency to update Status bar color

--- app/src/main/java/com/example/catexplorer/main/MainActivity.kt
+++ app/src/main/java/com/example/catexplorer/main/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.catexplorer.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import com.example.catexplorer.MainScreenView
 import com.example.catexplorer.ui.theme.CatExplorerTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,7 +17,14 @@ class MainActivity : ComponentActivity() {
 
         super.onCreate(savedInstanceState)
         setContent {
-            CatExplorerTheme(darkTheme = true){
+
+            // Updates the status bar color
+            val systemUiController = rememberSystemUiController()
+            SideEffect {
+                systemUiController.setStatusBarColor(Color.DarkGray)
+            }
+
+            CatExplorerTheme(darkTheme = true) {
                 MainScreenView()
             }
 
