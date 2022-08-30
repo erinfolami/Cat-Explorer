685ff7524aa55e3bac96fc20d285d18501ce1bf6
erinfolami <emmanuelerinfolami2@gmail.com>
2022-08-30 20:46:52 +0100

    fixed favourite to save only unique data

--- app/src/main/java/com/example/catexplorer/data/local/FavouriteEntity.kt
+++ app/src/main/java/com/example/catexplorer/data/local/FavouriteEntity.kt
@@ -2,9 +2,17 @@ package com.example.catexplorer.data.local
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Favourites")
+
+@Entity(
+    tableName = "Favourites", indices = [Index(
+        value = ["image_Url"],
+        unique = true
+    )]
+)
+
 data class FavouriteEntity(
     @PrimaryKey(autoGenerate = true)
     var id: Int? = null,
