f036d19eb7832b28ea948fa9ae57437515f831b3
erinfolami <emmanuelerinfolami2@gmail.com>
2022-10-28 17:39:24 +0100

    implemeted share image

--- app/src/main/java/com/example/catexplorer/screens/wallpapers/WallpapersDetailScreen.kt
+++ app/src/main/java/com/example/catexplorer/screens/wallpapers/WallpapersDetailScreen.kt
@@ -1,18 +1,25 @@
 package com.example.catexplorer.screens.wallpapers
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.util.Log
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import coil.compose.AsyncImage
-
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.FabPosition
+import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.imageResource
+import androidx.core.content.FileProvider
+import coil.compose.AsyncImage
+import com.example.catexplorer.BuildConfig
 import com.example.catexplorer.R
 import com.example.catexplorer.main.viewmodel.MainViewModel
 import com.example.catexplorer.screens.favourite.model.GetFavourite
@@ -22,6 +29,13 @@ import com.example.catexplorer.screens.wallpapers.multifab.MultiFabItem
 import com.example.catexplorer.screens.wallpapers.multifab.MultiFabState
 import com.example.catexplorer.screens.wallpapers.multifab.MultiFloatingActionButton
 import com.example.catexplorer.screens.wallpapers.viewmodel.WallpapersSharedViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+
 
 @Composable
 fun WallpapersDetailScreen(
@@ -38,7 +52,7 @@ fun WallpapersDetailScreen(
 
     val favourite = wallpapersSharedViewModel.response.value.data
 
-
+    val coroutineScope = rememberCoroutineScope()
     val userId = mainViewModel.dataStoreData.value
 
 
@@ -85,7 +99,13 @@ fun WallpapersDetailScreen(
                 stateChanged = { state -> toState = state },
                 onFabItemClicked = { item ->
                     when (item.identifier) {
-                        FabIdentifier.FAVOURITE.name -> postFavouriteModel?.let { postFavourite(it, tag,wallpapersSharedViewModel) }
+                        FabIdentifier.FAVOURITE.name -> postFavouriteModel?.let {
+                            postFavourite(
+                                it,
+                                tag,
+                                wallpapersSharedViewModel
+                            )
+                        }
 
 
                         FabIdentifier.DELETE_FAVOURITE.name -> deleteFavourite(
@@ -102,7 +122,13 @@ fun WallpapersDetailScreen(
                             )
                         }
 //
-//                        FabIdentifier.SHARE.name -> TODO
+                        FabIdentifier.SHARE.name -> imageUrl?.let {
+                            shareImage(
+                                it,
+                                coroutineScope,
+                                context
+                            )
+                        }
 
                     }
                 }
@@ -150,7 +176,11 @@ fun ScreenContent(imageUrl: String) {
     }
 }
 
-private fun postFavourite(postFavouriteModel: PostFavourite, tag: String, viewModel: WallpapersSharedViewModel){
+private fun postFavourite(
+    postFavouriteModel: PostFavourite,
+    tag: String,
+    viewModel: WallpapersSharedViewModel
+) {
     viewModel.postFavourite(postFavouriteModel)
     Log.i(tag, "added to favourite")
 }
@@ -167,9 +197,39 @@ private fun deleteFavourite(
     }
 }
 
-private fun onShowDialog(showDialog: MutableState<Boolean>){
+private fun onShowDialog(showDialog: MutableState<Boolean>) {
     showDialog.value = true
 }
 
+
+fun shareImage(imageUrl: String, scope: CoroutineScope, context: Context) {
+
+    scope.launch {
+        withContext(Dispatchers.IO) {
+
+
+            val cachePath = File(context.cacheDir, "images")
+            cachePath.mkdirs() // don't forget to make the directory
+            val stream =
+                FileOutputStream("$cachePath/image.png") // overwrites this image every time
+            getBitmap("TAG", context, imageUrl)?.compress(Bitmap.CompressFormat.PNG, 100, stream)
+            stream.close()
+
+            val imagePath = File(context.cacheDir, "images")
+            val newFile = File(imagePath, "image.png")
+            val contentUri: Uri = FileProvider.getUriForFile(
+                context,
+                "${BuildConfig.APPLICATION_ID}.provider",
+                newFile
+            )
+
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "image/*"
+            intent.putExtra(Intent.EXTRA_STREAM, contentUri)
+            context.startActivity(Intent.createChooser(intent, null))
+
+        }
+    }
+}
 
 
